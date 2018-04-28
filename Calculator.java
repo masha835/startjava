743905b651102d0fdc88f7ea354bc96109e67f64
masha835 <masha8353@gmail.com>
2018-04-28 13:32:23 +0200

    first commit2

---
+++ Calculator.java
@@ -0,0 +1,18 @@
+public class Calculator {
+public static void main(String args[]){
+	int a = 5;
+	int b = 6;
+	double c = 5.0;
+	double d = 6.0;
+	System.out.println(a+b);
+	System.out.println(a-b);
+	System.out.println(c*d);
+	System.out.println(c/d);
+	//System.out.println();//
+        System.out.println(c/100);
+	System.out.println(Math.pow(c,d));
+	
+ 
+
+ }
+}
