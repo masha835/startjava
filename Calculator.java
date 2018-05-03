3128e9852fc598702e68020d35055eb3753cbee3
masha835 <masha8353@gmail.com>
2018-05-03 22:45:59 +0200

    first commit

--- Calculator.java
+++ Calculator.java
@@ -4,6 +4,11 @@ public static void main(String args[]){
 	int b = 6;
 	double c = 5.0;
 	double d = 6.0;
+	if(a<0 || b<0) {
+	System.out.println("Вы ввели недопустимое значение. Запросите ответ без отрицательного знака");
+} else {
+	
+	
 	System.out.println(a+b);
 	System.out.println(a-b);
 	System.out.println(c*d);
@@ -11,7 +16,9 @@ public static void main(String args[]){
 	//System.out.println();//
         System.out.println(c/100);
 	System.out.println(Math.pow(c,d));
-	
+	System.out.println(a%b);
+
+	}
  
 
  }
